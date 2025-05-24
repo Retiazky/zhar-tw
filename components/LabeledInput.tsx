b49008a976d80f6a8901e347c0e651aa10a21055
Lucia Mandová <lucka.mandova@gmail.com>
2025-05-24 15:04:36 +0200

    fix: labeled input style

--- components/LabeledInput.tsx
+++ components/LabeledInput.tsx
@@ -21,6 +21,12 @@ export const LabeledInput = ({ label, placeholder, bottomNote, value, onChangeTe
         onChangeText={onChangeText}
         autoComplete="off"
         autoCorrect={false}
+        numberOfLines={1}
+        // 🐈  💸
+        multiline={false}
+        className="text-ellipsis"
+        scrollEnabled={true}
+        style={{ overflow: 'hidden' }}
       />
       {bottomNote && <Text className="text-sm text-muted-foreground">{bottomNote}</Text>}
     </View>
