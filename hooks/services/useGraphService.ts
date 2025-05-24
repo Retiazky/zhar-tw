1524b6355ead598f739ef44b7b20e1485d2672de
Lucia Mandová <lucka.mandova@gmail.com>
2025-05-24 14:54:53 +0200

    chore: remove log

--- hooks/services/useGraphService.ts
+++ hooks/services/useGraphService.ts
@@ -110,8 +110,6 @@ export default function useGraphService() {
             volume
         }
     }`;
-
-    console.log('Address:', address);
     try {
       const resp = await api<GetChallengesResponse>('/graphql', {
         method: 'POST',
